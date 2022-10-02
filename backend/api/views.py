99d901c3079d2dce1ba80ec1c15eff47482f7ddc
Jack Perry <jack.e.perry@outlook.com>
2022-10-02 13:17:25 -0600

    👌 IMPROVE: progress on course

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,15 +1,17 @@
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().order_by("?").first()
+    serializer = ProductSerializer(data=request.data)
 
-    data = {}
+    if serializer.is_valid(raise_exception=True):
+        instance = serializer.save()
+        print(instance)
 
-    if model_data:
-        data = model_to_dict(model_data, fields=["id", "title", "price"])
-
-    return JsonResponse(data)
+        return Response(serializer.data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,9 +3,9 @@ import requests
 
 endpoint = "http://127.0.0.1:8000/api/"
 
-get_response = requests.get(
+get_response = requests.post(
     endpoint,
     params={"abc": 123},
-    json={"query": "Hello World"}
+    json={"title": "Poop World"}
 )
-print(get_response.json())
+print(get_response.text[:1000])
